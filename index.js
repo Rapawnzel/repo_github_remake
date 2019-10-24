8a3ee759aea06ec89c91698440f7b2d7ae6864d4
n i n a <anndree.tg@gmail.com>
2019-10-24 07:45:55 +0200

    github basic - only js no styling

---
+++ index.js
@@ -0,0 +1,36 @@
+//  Rapawnzel
+
+function main(){
+    let username = document.querySelector("#user").value;
+    httpGet(`https://api.github.com/users/${username}`, 
+    (user) => {
+        let userObj = JSON.parse(user);
+        
+        document.querySelector("#userImg").innerHTML = `<img src = ${userObj["avatar_url"]} class = "img-fluid rounded">`;
+        document.querySelector("#username").innerHTML = userObj["login"];
+        document.querySelector("#name").innerHTML = userObj["name"];
+        document.querySelector("#bio").innerHTML = userObj["bio"];
+        httpGet(userObj["repos_url"], 
+            (repos) => {
+                let repoList = JSON.parse(repos);
+                let repoPrint = [];
+                for (repo in repoList){
+                    document.querySelector("#results").innerHTML += `<div id="reponame">${repoList[repo]["name"]}</div> 
+                                                                     <div id="stars">${repoList[repo]["stargazers_count"]}</div>
+                                                                     <div id="forks">${repoList[repo]["forks"]}</div> <br>`;
+                }
+            }
+        )	
+    })
+}
+
+function httpGet(theUrl, callback) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl);
+    xmlHttp.setRequestHeader("X-Requested-With", "XMLHttpRequest")
+    xmlHttp.send();
+}
